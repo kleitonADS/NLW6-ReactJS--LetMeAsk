06ab296f9a1657f94aa5947e93cb2f98cdce44ac
Kleiton Silva Souza de Azevedo <kleiton.ads@hotmail.com>
2021-06-23 19:53:38 -0400

    commit lesson 02
    add files assents

---
+++ frontend/src/pages/Home.tsx
@@ -0,0 +1,12 @@
+import illustrationImg from '../assets/images/illustration.svg'
+
+export function Home(){
+  return(
+    <div>
+      <img src={illustrationImg} alt="questions and answers illustration"/>
+      <strong></strong>
+
+
+    </div>
+  )
+}
