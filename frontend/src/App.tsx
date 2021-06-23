56632f3538ec4e4994bece4ad73b1d2d11eca745
Kleiton Silva Souza de Azevedo <kleiton.ads@hotmail.com>
2021-06-23 14:18:47 -0400

    Commit Aula 01
    Create project and add Firebase

---
+++ frontend/src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './services/firebase';
+
+
+function App() {
+  return (
+    <div className="App">
+        Let Me Ask APP
+    </div>
+  );
+}
+
+export default App;
